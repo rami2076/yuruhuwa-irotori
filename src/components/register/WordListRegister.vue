6307c560a6a517ac477f3d1c901926c2a80d6b1d
rami2076 <sakusennx@gmail.com>
2023-04-28 15:14:27 +0900

    Vueのv-modelの機能の学習

---
+++ src/components/register/WordListRegister.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+// reactive 要素になった。
+const isUseDefaultWordList = ref<boolean>(false);
+
+
+</script>
+
+
+<!--v3からはDOMを複数おけるようになった！-->
+<template>
+
+    <div>
+        <p>デフォルト単語一覧を使用する</p>
+        <input type="checkbox" v-model="isUseDefaultWordList">
+        <p>テストパターン2</p>
+        <input type="checkbox" :checked="isUseDefaultWordList" @change="isUseDefaultWordList=!isUseDefaultWordList">
+        <p>テストパターン3</p>
+        <input type="checkbox" checked="checked">
+    </div>
+
+</template>
