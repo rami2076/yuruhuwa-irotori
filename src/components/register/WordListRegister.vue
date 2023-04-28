eb709916ac43192f32064ffa950bdb28f67408de
rami2076 <sakusennx@gmail.com>
2023-04-28 15:17:12 +0900

    v-modelとそうで無い場合に関するメモ

--- src/components/register/WordListRegister.vue
+++ src/components/register/WordListRegister.vue
@@ -15,7 +15,7 @@ const isUseDefaultWordList = ref<boolean>(false);
     <div>
         <p>デフォルト単語一覧を使用する</p>
         <input type="checkbox" v-model="isUseDefaultWordList">
-        <p>テストパターン2</p>
+        <p>外部コンポーネントを使用する場合はv-modelを使用しないケースが多い</p>
         <input type="checkbox" :checked="isUseDefaultWordList" @change="isUseDefaultWordList=!isUseDefaultWordList">
         <p>テストパターン3</p>
         <input type="checkbox" checked="checked">
